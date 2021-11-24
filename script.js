b59eb2e247711e0a317151690e14d4d2c19b8fcb
Hamza-Mujahid <hamza.mujahid.hussain@gmail.com>
2021-11-24 21:27:17 +0500

    counter

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // console.log('hello')
 let a = 0;
+
 let counter = () => {
     // console.log(document.getElementById('cnt').innerHTML);
     a = a+1
-    document.getElementById('cnt').innerHTML= a;
+    document.getElementById('cnt').innerHTML = a;
+}
+let reduce= () => {
+    a = a-1
+    document.getElementById('cnt').innerHTML = a;
 }
