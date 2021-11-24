75641e30750bc44e4b14128bb8d3e36f544835f1
Hamza-Mujahid <hamza.mujahid.hussain@gmail.com>
2021-11-24 21:10:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 let a = 0;
 let counter = () => {
     console.log(document.getElementById('cnt').innerHTML);
-    // a= a+1
-    // document.getElementById('cnt').innerHTML= a;
+    a= a+1
+    document.getElementById('cnt').innerHTML= a;
 
 
 
